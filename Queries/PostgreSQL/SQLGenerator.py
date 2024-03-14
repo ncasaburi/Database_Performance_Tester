34c79211f84a92cd32a180dde0b338eb29a20622
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-03-13 21:22:33 -0300

    updating query files

--- Queries/PostgreSQL/SQLGenerator.py
+++ Queries/PostgreSQL/SQLGenerator.py
@@ -4,12 +4,20 @@ from faker import Faker
 import random
 import zipfile
 import io
+import os
+
+####################### Variables ###########################
+
+data_language = "en_US"
+number_of_rows = 500000
+
+#############################################################
 
 faker = Faker()
 faker.seed_instance(42)  # Set data consistency
 
 # Setting locale
-faker = Faker('en_US')
+faker = Faker(data_language)
 
 # Generate patients
 def generate_patient():
@@ -18,7 +26,7 @@ def generate_patient():
         'name': faker.first_name(),
         'surname': faker.last_name(),
         'birthday': faker.date_of_birth(minimum_age=18, maximum_age=90),
-        'gender': random.choice(['Masculino', 'Femenino']),
+        'gender': random.choice(['Male', 'Female']),
         'address': faker.street_address(),
         'city': faker.city(),
         'state': faker.state(),
@@ -45,8 +53,6 @@ def generate_doctor():
         'profession': random.choice(['Cardiology', 'Pediatrics', 'Gynecology', 'Ophthalmology', 'Dermatology']),
     }
 
-number_of_rows = 100000
-
 # Generate patients
 print("Generating patients...")
 patients = [generate_patient() for _ in range(number_of_rows)]
@@ -69,22 +75,27 @@ for i in perm:
 
 # Creation of SQL files in order to populate tables
 
+# if the path doesn't exist
+current_path = "Queries/PostgreSQL/Inserts/"+str(number_of_rows)+os.path.sep
+if not os.path.exists(current_path):
+    os.makedirs(current_path)
+
 print("Creating Zip file for patients...")
-with zipfile.ZipFile('Queries/PostgreSQL/Inserts/'+str(number_of_rows)+'_Patients.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+with zipfile.ZipFile(current_path+str(number_of_rows)+'_Patients.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     patients_in_memory = ""
     for patient in patients:
         patients_in_memory = patients_in_memory + (f"INSERT INTO patients (id_patient, name, surname, birthday, gender, address, city, state, phone) VALUES ('{patient['id_patient']}','{patient['name']}', '{patient['surname']}', '{patient['birthday']}', '{patient['gender']}', '{patient['address']}', '{patient['city']}', '{patient['state']}', '{patient['phone']}');\n")
     zipf.writestr(str(number_of_rows)+'_Patients.sql', io.BytesIO(patients_in_memory.encode()).getvalue())
 
 print("Creating Zip file for doctors...")
-with zipfile.ZipFile('Queries/PostgreSQL/Inserts/'+str(number_of_rows)+'_Doctors.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+with zipfile.ZipFile(current_path+str(number_of_rows)+'_Doctors.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     doctors_in_memory = ""
     for doctor in doctors:
         doctors_in_memory = doctors_in_memory + (f"INSERT INTO doctors (id_doctor, name, surname, profession) VALUES ('{doctor['id_doctor']}','{doctor['name']}', '{doctor['surname']}', '{doctor['profession']}');\n")
     zipf.writestr(str(number_of_rows)+'_Doctors.sql', io.BytesIO(doctors_in_memory.encode()).getvalue())
 
 print("Creating Zip file for the relationships among patients, doctors and medical records...")
-with zipfile.ZipFile('Queries/PostgreSQL/Inserts/'+str(number_of_rows)+'_PatientDoctorMedicalRecord.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+with zipfile.ZipFile(current_path+str(number_of_rows)+'_PatientDoctorMedicalRecord.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     patient_doctor_medical_records_in_memory = ""
     for record in medical_records:
         patient_doctor_medical_records_in_memory = patient_doctor_medical_records_in_memory + (f"INSERT INTO patient_doctor_medical_record (id_patient, id_doctor, id_medical_record) VALUES ('{patient_doctor[0][0]['id_patient']}','{patient_doctor[0][1]['id_doctor']}','{record['id_medical_record']}');\n")
@@ -93,7 +104,7 @@ with zipfile.ZipFile('Queries/PostgreSQL/Inserts/'+str(number_of_rows)+'_Patient
     zipf.writestr(str(number_of_rows)+'_PatientDoctorMedicalRecord.sql', io.BytesIO(patient_doctor_medical_records_in_memory.encode()).getvalue())
 
 print("Creating Zip file for medical records...")
-with zipfile.ZipFile('Queries/PostgreSQL/Inserts/'+str(number_of_rows)+'_MedicalRecords.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+with zipfile.ZipFile(current_path+str(number_of_rows)+'_MedicalRecords.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
     medical_records_in_memory = ""
     for record in medical_records:
         medical_records_in_memory = medical_records_in_memory + (f"INSERT INTO medical_records (id_medical_record, id_patient, admission_date, discharge_date, diagnosis, treatment, test_results) VALUES ({record['id_medical_record']}, {record['id_patient']},'{record['admission_date']}', '{record['discharge_date']}', '{record['diagnosis']}', '{record['treatment']}', '{record['test_result']}');\n")
