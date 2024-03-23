c97eddf2b6cc2a3aac4fcf7e08662110caac640d
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-03-23 16:19:36 -0300

    Adding Row delete option and logic for Postgres

--- src/menu/SubmenuPostgresRow.py
+++ src/menu/SubmenuPostgresRow.py
@@ -1,6 +1,8 @@
 from consolemenu import *
 from consolemenu.items import *
 from src.menu.SubmenuPostgresRowInsert import SubmenuPostgresRowInsert
+from src.menu.SubmenuPostgresRowUpdate import SubmenuPostgresRowUpdate
+from src.menu.SubmenuPostgresRowDelete import SubmenuPostgresRowDelete
 from src.menu.status import status
 
 class SubmenuPostgresRow():
@@ -13,8 +15,8 @@ class SubmenuPostgresRow():
         
         #submenu items
         postgres_row_insert = SubmenuItem("Insert rows", SubmenuPostgresRowInsert().get(), self.submenu_postgres_row)
-        postgres_row_update = SubmenuItem("Update rows", SubmenuPostgresRowInsert().get(), self.submenu_postgres_row)
-        postgres_row_delete = SubmenuItem("Delete rows", SubmenuPostgresRowInsert().get(), self.submenu_postgres_row)
+        postgres_row_update = SubmenuItem("Update rows", SubmenuPostgresRowUpdate().get(), self.submenu_postgres_row)
+        postgres_row_delete = SubmenuItem("Delete rows", SubmenuPostgresRowDelete().get(), self.submenu_postgres_row)
 
         #submenu appends
         self.submenu_postgres_row.append_item(postgres_row_insert)
