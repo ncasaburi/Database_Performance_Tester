66fe29948b3b3d8ff5fc6d4e915d61657b6d6482
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-03-24 20:56:08 -0300

    Updating Mongo Submenus

---
+++ src/menu/mongo/SubmenuMongoDatabaseDrop.py
@@ -0,0 +1,45 @@
+from consolemenu import *
+from consolemenu.items import *
+from src.drivers.MongoDBDriver import MongoDB
+from src.config.Config import Config
+from src.menu.status import status
+from src.logger.SingleLogger import SingleLogger
+
+class SubmenuMongoDatabaseDrop():
+
+    def __init__(self) -> None:
+        """This function initializes the SubmenuMongoDatabaseDrop class"""
+        
+        #submenu definition
+        self.submenu_mongo_database_drop = ConsoleMenu("MongoDB Database Drop", status)
+        
+        #submenu items
+        mongo_database_drop_default = FunctionItem("Drop default database", self.mongo_database_drop_fn, args=["default"]) 
+        mongo_database_drop_current = FunctionItem("Drop current database", self.mongo_database_drop_fn, args=["current"]) 
+        mongo_database_drop_custom = FunctionItem("Drop a custom database", self.mongo_database_drop_fn, args=["custom"])
+
+        #submenu appends
+        self.submenu_mongo_database_drop.append_item(mongo_database_drop_default)
+        self.submenu_mongo_database_drop.append_item(mongo_database_drop_current)
+        self.submenu_mongo_database_drop.append_item(mongo_database_drop_custom)
+
+    def get(self) -> ConsoleMenu:
+        return self.submenu_mongo_database_drop
+    
+    def mongo_database_drop_fn(self, type:str):
+        """This function allows the user to drop a MongoDB database"""
+
+        try:
+            mongo = MongoDB()
+            if type == "default":
+                mongo.drop(Config().default_dbs["default_mongo_connection_string"],Config().default_dbs["default_database_name"])
+            elif type == "current":
+                mongo.drop(mongo.connection_string,mongo.status())
+            else:
+                print("Enter the connection string: (example: mongodb://user:password@localhost:27017/)")
+                postgre_connection_string = input()
+                print("Enter the database name:")
+                db_name = input()
+                mongo.drop(postgre_connection_string,db_name)
+        except:
+            SingleLogger().logger.exception("Error while dropping a MongoDB database", exc_info=True)
