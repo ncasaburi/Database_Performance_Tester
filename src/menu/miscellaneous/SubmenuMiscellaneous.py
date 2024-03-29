25acb3f69363ac1081c85d026aa13ca662d6c0f6
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-03-29 00:45:21 -0300

    Uploading new menus for Miscellaneous

--- src/menu/miscellaneous/SubmenuMiscellaneous.py
+++ src/menu/miscellaneous/SubmenuMiscellaneous.py
@@ -1,7 +1,8 @@
 from consolemenu import *
 from consolemenu.items import *
 from src.menu.miscellaneous.SubmenuMiscellaneousGenerator import SubmenuMiscellaneousGenerator
-from src.menu.status import status
+from src.logic.status import status
+from src.logic.miscellaneous.miscellaneous_autoconnect import *
 
 class SubmenuMiscellaneous():
 
@@ -13,10 +14,19 @@ class SubmenuMiscellaneous():
 
         #submenu items
         miscellaneous_generator = SubmenuItem("Generate data to populate databases", SubmenuMiscellaneousGenerator().get(), menu=self.submenu_miscellaneous)
+        miscellaneous_autoconnect = FunctionItem((lambda condition: "Deactivate autoconnect" if condition else "Activate autoconnect")(autoconnect_read()), self.miscellaneous_autoconnect_update_fn)
 
         #submenu appends
         self.submenu_miscellaneous.append_item(miscellaneous_generator)
+        self.submenu_miscellaneous.append_item(miscellaneous_autoconnect)
     
     def get(self) -> ConsoleMenu:
         return self.submenu_miscellaneous
-         
+
+    def miscellaneous_autoconnect_update_fn(self) -> None:
+        autoconnect_switch()
+        self.submenu_miscellaneous.remove_item(self.submenu_miscellaneous.items[1])
+        miscellaneous_autoconnect = FunctionItem((lambda condition: "Deactivate autoconnect" if condition else "Activate autoconnect")(autoconnect_read()), self.miscellaneous_autoconnect_update_fn)
+        self.submenu_miscellaneous.append_item(miscellaneous_autoconnect)
+        self.submenu_miscellaneous.show()
+        
--- src/menu/miscellaneous/SubmenuMiscellaneousGenerator.py
+++ src/menu/miscellaneous/SubmenuMiscellaneousGenerator.py
@@ -1,10 +1,7 @@
 from consolemenu import *
 from consolemenu.items import *
-from src.config.Config import Config
-from src.menu.status import status
-from src.logger.SingleLogger import SingleLogger
-from src.generator.DataGenerator import DataGenerator
-import os
+from src.logic.status import status
+from src.logic.miscellaneous.miscellaneous_generator import miscellaneous_generator_fn
 
 class SubmenuMiscellaneousGenerator():
 
@@ -15,9 +12,9 @@ class SubmenuMiscellaneousGenerator():
         self.submenu_miscellaneous_generator = ConsoleMenu("Data Generator", status)
         
         #submenu items
-        miscellaneous_generator_both = FunctionItem("Generate data to populate both SQL and NoSQL databases", self.miscellaneous_generator_fn, args=[True,True]) 
-        miscellaneous_generator_sql = FunctionItem("Generate data to populate SQL databases", self.miscellaneous_generator_fn, args=[True,False])
-        miscellaneous_generator_mql = FunctionItem("Generate data to populate NoSQL databases", self.miscellaneous_generator_fn, args=[False,True])
+        miscellaneous_generator_both = FunctionItem("Generate data to populate both SQL and NoSQL databases", miscellaneous_generator_fn, args=[True,True]) 
+        miscellaneous_generator_sql = FunctionItem("Generate data to populate SQL databases", miscellaneous_generator_fn, args=[True,False])
+        miscellaneous_generator_mql = FunctionItem("Generate data to populate NoSQL databases", miscellaneous_generator_fn, args=[False,True])
 
         #submenu appends
         self.submenu_miscellaneous_generator.append_item(miscellaneous_generator_both)
@@ -26,35 +23,3 @@ class SubmenuMiscellaneousGenerator():
 
     def get(self) -> ConsoleMenu:
         return self.submenu_miscellaneous_generator
-    
-    def miscellaneous_generator_fn(self, SQL_enable, MQL_enable):
-        """This function allows the user to generate data to populate SQL and NoSQL databases"""
-
-        try:
-            ready = False
-            while not ready:
-                os.system('clear')
-                print("Data generator\n\n")
-                total_rows = int(input("How many rows do you want to generate?: "))
-                file_numbers = int(input("How many files would you like the rows to be split into?: "))
-                if total_rows == "":
-                    total_rows = 100000
-                if file_numbers == "":
-                    file_numbers = 10
-                print("")
-                print(f"As a result, {total_rows} rows are going to be generated and split into {file_numbers} files")
-                confirmation = input("Are you sure? [yes,no,exit]: ").lower()
-                if confirmation == "exit":
-                    return
-                if confirmation == "yes" or confirmation == "y":
-                    ready = True
-            os.system('clear')
-            print("Data generator\n")
-            generator = DataGenerator(int(total_rows/file_numbers),file_numbers, SQL_enable, MQL_enable)
-            generator.generate_patients()
-            generator.generate_doctors()
-            generator.generate_medicalrecords()
-            print("\nPress enter to continue...")
-            input()
-        except:
-            SingleLogger().logger.exception("Error while generating data with DataGenerator", exc_info=True)
