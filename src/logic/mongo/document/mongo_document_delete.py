157337cd4afc3908d62707c554ffc5258a137315
jccascoudc <jccasco@udc.edu.ar>
2024-03-31 10:42:17 -0300

    Se agregan document_delete_custom y document_uptdate_custom

--- src/logic/mongo/document/mongo_document_delete.py
+++ src/logic/mongo/document/mongo_document_delete.py
@@ -28,32 +28,19 @@ def mongo_document_delete_fn(type:str) -> None:
             MongoDB().execute_query_delete('patients',query_delete)
             query_delete = {"id_medical_record": {"$gte": id_minimo, "$lte": id_maximo}}
             MongoDB().execute_query_delete('medical_records',query_delete)
-
-            # print("How many rows do you want to delete?: ("+str(postgres.run_query("SELECT COUNT(*) FROM doctors","",expected_result=True)[0][0])+" rows left)")
-            # rows_to_delete = input()
-            # postgres.run_query("DELETE FROM doctor_medical_records WHERE id_doctor IN ( SELECT id_doctor FROM doctor_medical_records ORDER BY id_medical_record DESC, id_doctor DESC LIMIT "+str(rows_to_delete)+" )","Deleting "+str(rows_to_delete)+" relationships between doctors and medical records...")
-            # postgres.run_query("DELETE FROM medical_records WHERE id_medical_record IN ( SELECT id_medical_record FROM medical_records ORDER BY id_patient DESC, id_medical_record DESC LIMIT "+str(rows_to_delete)+" )","Deleting "+str(rows_to_delete)+" medical records...")
-            # postgres.run_query("DELETE FROM patients WHERE id_patient IN ( SELECT id_patient FROM patients ORDER BY id_patient DESC LIMIT "+str(rows_to_delete)+" )","Deleting "+str(rows_to_delete)+" patients...")
-            # postgres.run_query("DELETE FROM doctors WHERE id_doctor IN ( SELECT id_doctor FROM doctors ORDER BY id_doctor DESC LIMIT "+str(rows_to_delete)+" )","Deleting "+str(rows_to_delete)+" doctors...")
         else:
-            # table_list = postgres.run_query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_catalog = '"+postgres.status()+"';", expected_result=True)
-            # print("Tables:\n")
-            # for table in table_list:
-            #     print(" - "+str(table[0])+" (rows: "+str(postgres.run_query("SELECT COUNT(*) FROM "+str(table[0]),"",expected_result=True)[0][0])+", columns: "+str(postgres.run_query("SELECT COUNT(*) FROM information_schema.columns WHERE table_name = '"+str(table[0])+"';","",expected_result=True)[0][0])+")")
-            #     print("   ("+', '.join(item[0] for item in postgres.run_query("SELECT column_name FROM information_schema.columns WHERE table_name = '"+str(table[0])+"'","",expected_result=True))+")")
-            # print("")
-            # print("Enter your query to delete rows:\n")
-            # print("  Example:")
-            # print("    DELETE FROM tablename")
-            # print("    WHERE condition")
-            # tablename = input("\n\n  DELETE FROM ")
-            # condition = input("  WHERE ")
-            # query = ""
-            # if not tablename == "":
-            #     query = "DELETE FROM "+tablename
-            # if not condition == "":
-            #     query = query + " " + "WHERE "+ condition
-            # postgres.run_query(query)
-            print("Collection")
+            print("Enter your query to delete documents: (if a field doesn't apply, press enter)\n")
+            print("  Example:")
+            print("    db.collectionname")
+            print("    delete")
+            print('    filter.{"field1": {"$gte": value1, "$lte": value2}}')
+            collectionname = input("\n\n  db.")
+            print("    delete")
+            query_update = input("filter.")
+            if not collectionname == "" and not query_update == "":
+                MongoDB().execute_query_delete(collectionname,query_update)
+            else:
+                print("Some of the inputs are empty...")
+                input("")
     except:
         SingleLogger().logger.exception("Error while deleting documents to MongoDB", exc_info=True)
--- src/logic/mongo/document/mongo_document_update.py
+++ src/logic/mongo/document/mongo_document_update.py
@@ -30,43 +30,21 @@ def mongo_document_update_fn(type:str):
             query_update = {"id_medical_record": {"$gte": id_minimo, "$lte": id_maximo}}
             update = {"$set": {"discharge_date": datetime.now().strftime('%Y-%m-%d')}}
             MongoDB().execute_query_update('medical_records',query_update,update)
-                
-            # # Realizar la actualización
-            # result = collection.update_many({"id_patient": {"$gte": id_minimo, "$lte": id_maximo}},
-            #                      {"$set": {"name": "Roberts"}})
-            #print("Total documents updated:", result.modified_count)
-
-            # collection_default = 'doctors'
-            # collection = MongoDB().db[collection_default]
-            # result = collection.update_many({"id_doctor": {"$gte": id_minimo, "$lte": id_maximo}},
-            #                      {"$set": {"name": "Mark"}})
-            # print("Total documents updated:", result.modified_count)
-
-            # postgres.run_query("UPDATE medical_records SET discharge_date = '"+datetime.now().strftime('%Y-%m-%d')+"' WHERE id_medical_record IN ( SELECT id_medical_record FROM medical_records ORDER BY id_medical_record DESC LIMIT "+str(rows_to_update)+")","Updating "+str(rows_to_update)+" medical records...")
-            # postgres.run_query("UPDATE doctors SET name = 'Mark' WHERE id_doctor IN ( SELECT id_doctor FROM doctors ORDER BY id_doctor DESC LIMIT "+str(rows_to_update)+" )", "Updating "+str(rows_to_update)+" doctors...")
-
         else:
-            # table_list = postgres.run_query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_catalog = '"+postgres.status()+"';", expected_result=True)
-            print("Tables:\n")
-    #         for table in table_list:
-    #             print(" - "+str(table[0])+" (rows: "+str(postgres.run_query("SELECT COUNT(*) FROM "+str(table[0]),"",expected_result=True)[0][0])+", columns: "+str(postgres.run_query("SELECT COUNT(*) FROM information_schema.columns WHERE table_name = '"+str(table[0])+"';","",expected_result=True)[0][0])+")")
-    #             print("   ("+', '.join(item[0] for item in postgres.run_query("SELECT column_name FROM information_schema.columns WHERE table_name = '"+str(table[0])+"'","",expected_result=True))+")")
-    #         print("")
-    #         print("Enter your query to update rows: (if a field doesn't apply, press enter)\n")
-    #         print("  Example:")
-    #         print("    UPDATE tablename")
-    #         print("    SET column1 = 'value1', column2 = 'value2'")
-    #         print("    WHERE condition")
-    #         tablename = input("\n\n  UPDATE ")
-    #         set = input("  SET ")
-    #         condition = input("  WHERE ")
-    #         query = ""
-    #         if not tablename == "":
-    #             query = "UPDATE "+tablename
-    #         if not set == "":
-    #             query = query + " " + "SET "+set
-    #         if not condition == "":
-    #             query = query + " " + "WHERE "+ condition
-    #         postgres.run_query(query,"Updating rows with custom query...")
+            print("Enter your query to update documents: (if a field doesn't apply, press enter)\n")
+            print("  Example:")
+            print("    db.collectionname")
+            print("    update.")
+            print('    filter.{"field1": {"$gte": value1, "$lte": value2}}')
+            print('    set.{"$set": {"field2": value}}')
+            collectionname = input("\n\n  db.")
+            print("    update.")
+            query_update = input("filter.")
+            update = input("set.")
+            if not collectionname == "" and not query_update == "" and not update == "":
+                MongoDB().execute_query_update(collectionname,query_update,update)
+            else:
+                print("Some of the inputs are empty...")
+                input("")
     except:
         SingleLogger().logger.exception("Error while updating rows on PostgreSQL", exc_info=True)
