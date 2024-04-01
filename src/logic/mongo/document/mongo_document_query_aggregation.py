58684c9576f3156f3eed7cd523bca7c8188f3f43
Nicolás Casaburi <casaburinicolaslp@hotmail.com>
2024-04-01 16:23:35 -0300

    Improving logic for aggregation on mongo

--- src/logic/mongo/document/mongo_document_query_aggregation.py
+++ src/logic/mongo/document/mongo_document_query_aggregation.py
@@ -6,19 +6,38 @@ from src.logic.mongo.collection.mongo_collection_list import mongo_collection_li
 import ast
 import os
 import re
+import json
 
 def mongo_document_query_aggregation_fn(type:str):
     """This function allows the user to perform queries with aggregation on a MongoDB database"""
 
     try:
         mongo = MongoDB()
-        if type == "default":
+        if type == "default1":
             print("The following query is going to be executed:\n")
             collection_name = 'doctors'
             query = '[ {"$match": {"name": "Michael"}}, {"$limit": 5} ]'
             print("  collection = "+collection_name)
             print("  pipeline = "+query)
-            result = mongo.execute_aggregate(collection_name, ast.literal_eval(query), f"Executing aggregation on collection: {collection_name}")
+            result = mongo.execute_aggregate(collection_name, ast.literal_eval(query), "Executing default aggregation query1")
+            print("\nResult:\n")
+            print("  "+'\n  '.join(map(str, result)))
+            print("\n\nPress enter to continue...")
+            input()
+        elif type == "default2":
+            print("The following query is going to be executed:\n")
+            content_mql = Zipper().unzip_content(Config().default_data["default_mongo_queries"]+"Query1.zip","js")
+            print("db.patients.aggregate("+content_mql+")")
+            result = mongo.execute_aggregate("patients",json.loads(content_mql),"Executing default aggregation query2")
+            print("\nResult:\n")
+            print("  "+'\n  '.join(map(str, result)))
+            print("\n\nPress enter to continue...")
+            input()
+        elif type == "default3":
+            print("The following query is going to be executed:\n")
+            content_mql = Zipper().unzip_content(Config().default_data["default_mongo_queries"]+"Query2.zip","js")
+            print("db.patients.aggregate("+content_mql+")")
+            result = mongo.execute_aggregate("patients",json.loads(content_mql),"Executing default aggregation query3")
             print("\nResult:\n")
             print("  "+'\n  '.join(map(str, result)))
             print("\n\nPress enter to continue...")
